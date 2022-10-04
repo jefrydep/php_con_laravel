8a35cd9d805929b909115f5273d86e23bf351d2d
jefry <jefrydep@gmail.com>
2022-10-04 16:15:17 -0500

    agregando mas cambios creando ontroladores

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class indexController extends Controller
+class IndexController extends Controller
 {
-    //
+    public function __invoque(){
+        return 'hola bienvenidos al curso de laravel';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\indexControler;
 
 
 
 
 
+Route::get('/', [indexController::class, ]);
 
 Route::get('/', function () {
     return'bienvenido mi primera aplicaion con larabel';
@@ -14,5 +16,3 @@ Route::get('/', function () {
 Route:: get('productos', function(){
     return'Bienvenidio a la seccin de los productos';
 });
-
- 
